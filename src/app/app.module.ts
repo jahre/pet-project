4f3d66d805fd5f7885d0b8d58ebcdeaf4ffd7e38
jahre <jahre@tut.by>
2018-08-01 22:46:48 +0300

    dev1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { GrouppyComponent } from './grouppy/grouppy.component';
 import { ChangedBrieflyComponent } from './changed-briefly/changed-briefly.component';
+import { Dev1Component } from './dev1/dev1.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     GrouppyComponent,
-    ChangedBrieflyComponent
+    ChangedBrieflyComponent,
+    Dev1Component
   ],
   imports: [
     BrowserModule
