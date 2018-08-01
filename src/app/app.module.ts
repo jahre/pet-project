e6fed8a937f4b4706c73dd841480c049244f8f21
jahre <jahre@tut.by>
2018-08-01 23:15:53 +0300

    f2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { GrouppyComponent } from './grouppy/grouppy.component';
 import { ChangedBrieflyComponent } from './changed-briefly/changed-briefly.component';
 import { Dev1Component } from './dev1/dev1.component';
+import { Feature2Component } from './feature2/feature2.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { Dev1Component } from './dev1/dev1.component';
     AppComponent,
     GrouppyComponent,
     ChangedBrieflyComponent,
-    Dev1Component
+    Dev1Component,
+    Feature2Component
   ],
   imports: [
     BrowserModule
